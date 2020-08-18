6a41254a1b484b938588ec672b060f8254bde623
gmichalopulos <gregomicha@gmail.com>
2020-08-17 23:13:48 -0300

    determino ganador

--- src/index.js
+++ src/index.js
@@ -23,9 +23,16 @@ class Board extends React.Component {
         };
     }
 
+    getNextToken() {
+        return this.state.xIsNext ? 'X' : 'O';
+    }
+
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        if(calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.getNextToken();
         this.setState({
             squares: squares,
             xIsNext: !this.state.xIsNext,
@@ -42,7 +49,14 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if(winner) {
+            status = 'Winner: ' + winner;
+        }
+        else {
+            status = 'Next player: ' + this.getNextToken();
+        }
 
         return (
             <div>
@@ -86,4 +100,24 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById('root'));
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   
